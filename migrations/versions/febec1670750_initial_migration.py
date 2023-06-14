612213ceb1ba03271dad0143ca407e6a5df5be51
Pierce <henriksbopierce@gmail.com>
2023-06-14 16:51:57 -0700

    env fixed

--- migrations/versions/febec1670750_initial_migration.py
+++ migrations/versions/febec1670750_initial_migration.py
@@ -8,6 +8,9 @@ Create Date: 2023-06-14 14:38:46.497031
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = 'febec1670750'

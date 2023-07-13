d4d5fe16441c84a6a992a92549e2ba1678e67d58
Pierce <henriksbopierce@gmail.com>
2023-07-13 14:51:36 -0700

    fix

--- app/models/models.py
+++ app/models/models.py
@@ -136,3 +136,5 @@ bookmarks = db.Table(
     db.Column('user_id', db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), primary_key=True ),
     db.Column('post_id', db.Integer, db.ForeignKey(add_prefix_for_prod('posts.id')), primary_key=True )
 )
+if environment == "production":
+    bookmarks.schema = SCHEMA
